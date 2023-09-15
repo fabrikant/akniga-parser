ab3dfe715bf795dfbcba429f4a1b171a30dbcf6a
Artem Banderov <banderovaa@gmail.com>
2023-09-16 02:18:29 +0600

    gui

--- akniga_db_viewer.py
+++ akniga_db_viewer.py
@@ -1,8 +1,8 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic, QtCore, QtGui
-from PyQt5.Qt import QStandardItemModel, QStandardItem
-from PyQt5.QtCore import Qt
+from PyQt5.Qt import QStandardItemModel, QAbstractTableModel, QStandardItem
+from PyQt5.QtCore import Qt, QVariant
 import akniga_sql as sql
 
 
@@ -14,6 +14,40 @@ class FilterItem(QStandardItem):
         self.setCheckable(checkable)
         self.setEditable(False)
 
+class BooksTableModel(QAbstractTableModel):
+    def __init__(self, db_books=None):
+        QAbstractTableModel.__init__(self, None)
+        self.db_books = db_books
+        self.db_books_list = self.db_books.all()
+
+    def rowCount(self, parent):
+        return len(self.db_books_list)
+
+    def columnCount(self, parent):
+        return len(self.db_books.column_descriptions) - 1
+
+    def data(self, index, role):
+        if not index.isValid():
+            return QVariant()
+        elif role != Qt.DisplayRole:
+            return QVariant()
+        else:
+            return self.db_books_list[index.row()][index.column()]
+
+    def headerData(self, col, orientation, role):
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            return self.db_books.column_descriptions[col]['name']
+        elif orientation == Qt.Vertical and role == Qt.DisplayRole:
+            return col
+        else:
+            return QVariant()
+
+    def get_description(self, row):
+        return self.db_books_list[row][len(self.db_books.column_descriptions) - 1].description
+
+    def sort(self, Ncol, order):
+        print('sort', Ncol, order)
+
 
 class MainWindow(QMainWindow):
 
@@ -21,6 +55,9 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         uic.loadUi('ui/main.ui', self)
         self.books_constraints = set()
+
+
+
 
     def on_start_connecting(self):
         self.session = sql.get_session('sqlite:///akniga.db')
@@ -51,11 +88,6 @@ class MainWindow(QMainWindow):
                     yield child
                     if child.hasChildren():
                         yield from recurse(child)
-                    # for column in range(parent.columnCount()):
-                    #     child = parent.child(row, column)
-                    #     yield child
-                    #     if child.hasChildren():
-                    #         yield from recurse(child)
 
             if root is not None:
                 yield from recurse(root)
@@ -66,30 +98,37 @@ class MainWindow(QMainWindow):
                 self.books_constraints.update(item.db_object.books)
         self.get_data()
 
+    def on_book_row_selected(self, selection1, selection2):
+        description = self.table_books.model().get_description(selection1.first().indexes()[0].row())
+        self.description.setDocument(QtGui.QTextDocument(description))
+
     # Работа с данными
     def get_data(self):
-        table = self.tableBooks
-        table.setRowCount(0)
-
         # db_books = self.session.query(sql.Book).limit(1000).offset(0)
-        db_books = self.session.query(sql.Book)
+        db_books = self.session.query(sql.Book.title.label('Название'), sql.Author.name.label('Автор'),
+                                      sql.Book.duration.label('Продолжительность'),
+                                      sql.Performer.name.label('Исполнитель'), sql.Book.free.label('Бесп.'), sql.Book)
         if len(self.books_constraints):
             ids = []
             for book_constr in self.books_constraints:
                 ids.append(book_constr.book_id)
             db_books = db_books.filter(sql.Book.id.in_(ids))
-        tablerow = 0
-        for db_book in db_books:
-            table.setRowCount(tablerow+1)
-            table.setItem(tablerow, 0, QTableWidgetItem(db_book.title))
-            tablerow += 1
-            #table.model().index(1,1)
-            #table.model().data(table.model().index(1,0))
-            #table.model().columnCount()
-            # table.model().rowCount()
-            # table.model().headerData(1, )
-            # table.model().headerData(0, Qt.Orientation.Vertical, Qt.ItemDataRole.TextColorRole)
 
+        db_books = db_books.join(sql.Author).join(sql.Performer)
+        db_books = db_books.limit(1000).offset(0)
+
+        table = self.table_books
+
+        table.setModel(BooksTableModel(db_books))
+        table.resizeColumnsToContents()
+        table.resizeRowsToContents()
+        selectionModel = self.table_books.selectionModel()
+        selectionModel.selectionChanged.connect(self.on_book_row_selected)
+
+        # table.rowSpan(2,3)
+        # table.columnSpan(2)
+        table.setColumnWidth(0, (350 if table.columnWidth(0)%2 < 350 else (table.columnWidth(0) % 2)))
+        # print(table.columnWidth(0))
 
 
     def create_filter_items(self, session, parent_item, type_id, parent_id):
